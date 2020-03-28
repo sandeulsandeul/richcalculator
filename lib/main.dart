8f06eb4b80495efdc25a0a1d00e62d71677e8f95
sandeulsandeul <wjdtksdk@ewhain.net>
2020-03-28 23:42:56 +0900

    그림 추가 초기부터 다시 코드 작성중

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,25 @@
 import 'package:flutter/material.dart';
 
 //앱 시작 부분
-void main() {
-  runApp(
-    MaterialApp(
-      title : '부자 계산기',
-      home : RichApp(),
-    )
-  );
-}
+void main() => runApp(RichApp());
 
 //시작 클래스 머티리얼 디자인 앱 생성
-class RichApp extends StatefulWidget {
+class RichApp extends StatelessWidget {
   @override
-  State <StatefulWidget> createState() {
-    return _RichAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '부자 계산기',
+      home: RichAppPage(),
+    );
   }
 }
-
-class _RichAppState extends State<RichApp>{
+//시작 ㅡㄹ래스가 실제로 표시하는 클래스, 카운터 앱 화면
+class RichAppPage extends StatefulWidget{
+  @override
+  _RichAppState createState() => _RichAppState();
+}
+class _RichAppState extends State<RichAppPage>{
+  final _minimumPadding = 5.0;
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -27,17 +28,94 @@ class _RichAppState extends State<RichApp>{
           title :Text('라이프스타일 계산기'),
       ),
       body: Container(
+        margin: EdgeInsets.all(_minimumPadding *2),
         child: Column(
+          children: <Widget>[
+            getImageAsset(),
+            Padding(
+              padding : EdgeInsets.only(top:_minimumPadding , bottom: _minimumPadding),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: '주거비',
+                    hintText: '주거비 e.g. 2000000',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0)
+                    )
+                ),
+              ),
+            ),
+            Padding(
+              padding : EdgeInsets.only(top:_minimumPadding , bottom: _minimumPadding),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: '식비',
+                    hintText: '식비 e.g. 2000000',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0)
+                    )
+                ),
+              ),
+            ),
+            Padding(
+              padding : EdgeInsets.only(top:_minimumPadding , bottom: _minimumPadding),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: '생활비',
+                    hintText: '생활비( 차량유지비, 통신비 .. ) e.g. 2000000',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0)
+                    )
+                ),
+              ),
+            ),
+            Padding(
+              padding : EdgeInsets.only(top:_minimumPadding , bottom: _minimumPadding),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: '여가생활비',
+                    hintText: '여가생활비 e.g. 2000000',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0)
+                    )
+                ),
+              ),
+            ),
+            Padding(
+                padding : EdgeInsets.only(top:_minimumPadding , bottom: _minimumPadding),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: RaisedButton(
+                          child: Text('계산하기'),
+                          onPressed: (){
 
+                          },)
+                    ),
+                    Expanded(
+                        child: RaisedButton(
+                          child: Text('계산하기'),
+                          onPressed: (){
+
+                          },)
+                    ),
+                  ],
+                )
+            )
+          ],
         ),
       ),
     );
   }
+
+  Widget getImageAsset(){
+    AssetImage assetImage = AssetImage('images/그림.png');
+    Image image = Image(image: assetImage, width: 125.0, height: 125.0,);
+
+    return Container(child: image, margin: EdgeInsets.all(_minimumPadding*5),);
+  }
 }
 
-Widget getImageAsset(){
-  AssetImage assetImage = AssetImage('images/그림.png');
-  Image image = Image(image: assetImage, width: 125.0, height: 125.0,);
-
-  return Container(child: image,);
-}
