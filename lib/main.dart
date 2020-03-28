450c60a55eb881ae39174d29300f8db971f8b9d9
sandeulsandeul <wjdtksdk@ewhain.net>
2020-03-28 22:40:18 +0900

    그림 추가 초기부터 다시 코드 작성중

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
 //앱 시작 부분
-void main() => runApp(RichApp());
+void main() {
+  runApp(
+    MaterialApp(
+      title : '부자 계산기',
+      home : RichApp(),
+    )
+  );
+}
 
 //시작 클래스 머티리얼 디자인 앱 생성
 class RichApp extends StatefulWidget {
   @override
-  _RichAppState createState() => _RichAppState();
+  State <StatefulWidget> createState() {
+    return _RichAppState();
+  }
 }
 
 class _RichAppState extends State<RichApp>{
@@ -17,123 +26,18 @@ class _RichAppState extends State<RichApp>{
       appBar: AppBar(
           title :Text('라이프스타일 계산기'),
       ),
-      body: Padding( 
-        padding: const EdgeInsets.all(16.0),
+      body: Container(
         child: Column(
-          children: <Widget>[
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: '주거 비용',
-                hintText: '주거 비용 e.g. 1000000',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.0)
-                )
-              ),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: '식비',
-                  hintText: '식비 e.g. 1000000',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5.0)
-                  )
-              ),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: '차량 유지 비용',
-                  hintText: '차량 유지 비용 e.g. 1000000',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5.0)
-                  )
-              ),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: '쇼핑 비용',
-                  hintText: '쇼핑 비용 e.g. 1000000',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5.0)
-                  )
-              ),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: '여행 비용',
-                  hintText: '여행 비용 e.g. 1000000',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5.0)
-                  )
-              ),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: '여가생활 비용',
-                  hintText: '여가생활 비용 e.g. 1000000',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5.0)
-                  )
-              ),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: '용돈',
-                  hintText: '용돈 e.g. 1000000',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5.0)
-                  )
-              ),
-            ),
-          ],
-        ),
-      )
-    );
-  }
-}
-/*
-// 시작하는 클래스가 실제로 표시한는 클래스, 카운터 앱 화면
-class MyCustomForm extends StatefulWidget{
-  @override
-  _MyCustomFormState createState() => _MyCustomFormState();
-}
 
-//위 MyCustomForm 클래스의 상태를 나타내는 State 클래스
-class _MyCustomFormState extends State<MyCustomForm>{
-  //Textfield의 현재값을 얻는 데 필요
-  final myController = TextEditingController();
-  final myController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold( //머티리얼 디자인 기본 뼈대 위젯
-      appBar: AppBar( // 상단 App Bar
-        title: Text("부자되는 계산기"),
-      ),
-      body:
-        child: Column(
-          children: <Widget>[
-            Text("모든 금액은 1달 기준으로 입력해주세요"),
-            TextField(
-              controller: myController,
-              onChanged: (num){ //텍스트 변경 이벤트 감지
-                print("첫 번째 textfield:$text");
-              },
-            ),
-            TextField(
-
-            ),
-          ],
         ),
       ),
     );
   }
 }
-*/
+
+Widget getImageAsset(){
+  AssetImage assetImage = AssetImage('images/그림.png');
+  Image image = Image(image: assetImage, width: 125.0, height: 125.0,);
+
+  return Container(child: image,);
+}
